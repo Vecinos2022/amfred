c90fa2980aaa8a90b08d1e8669452daeb88c5d98
Emanuel Saucedo Olvera <emanuelsaucedo52@gmail.com>
2024-01-03 16:12:06 -0600

    fix login 3

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -47,7 +47,7 @@ const LoginPage = () => {
 
   useEffect(() => {
     if (session.status === 'authenticated') {
-      // router.push('/admin/dashboard')
+      router.push('/admin/dashboard')
       // signIn(undefined, { callbackUrl: '/admin/dashboard' })
       setUser(session.data?.user)
     }
@@ -58,7 +58,9 @@ const LoginPage = () => {
 
   return (
     <>
-      {session.status !== 'authenticated' ? (
+      {session.status === 'authenticated' ? (
+        <LoaderScreen />
+      ) : (
         <section className='relative flex flex-wrap lg:h-screen lg:items-center w-full'>
           <div className='w-screen h-screen flex items-center justify-center px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24 bg-default-200'>
             <div>
@@ -153,10 +155,6 @@ const LoginPage = () => {
             </Link>
           </div>
         </section>
-      ) : (
-        // console.log('LLEGA AQUÍ')
-        router.push('/admin/dashboard')
-        // console.log(session)
       )}
     </>
   )
