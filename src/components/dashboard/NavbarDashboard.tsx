a15ff25e391835bfbc683b7ef5cc8bb712a49f1e
Cordova Tapia Pablo Cesar <darckman201@gmail.com>
2024-01-04 09:24:24 -0600

    Prueba arreglo bug sesion

--- src/components/dashboard/NavbarDashboard.tsx
+++ src/components/dashboard/NavbarDashboard.tsx
@@ -9,15 +9,15 @@ import {
 import { signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import { FaChevronDown, FaUsers } from 'react-icons/fa'
+import { FaChevronDown } from 'react-icons/fa'
 import { IoClose, IoLogOut } from 'react-icons/io5'
 import { TfiMenu } from 'react-icons/tfi'
 
 import { useAuthBoundStore } from '@/store/auth/authSharedSlice'
+import Link from 'next/link'
 import LoaderScreen from '../shared/LoaderScreen'
 import LogoMain from '../shared/LogoMain'
 import Sidebar from './Sidebar'
-import Link from 'next/link'
 
 interface NavbarDashboardProps {
   children: React.ReactNode
@@ -57,56 +57,56 @@ const NavbarDashboard: React.FC<NavbarDashboardProps> = ({ children }) => {
       {session.status === 'loading' ? (
         <LoaderScreen />
       ) : (
-        session.status === 'authenticated' && (
-          <>
-            <nav className='fixed top-0 z-50 w-full bg-background border-solid border-gray-300 border-b-1 dark:bg-gray-800 dark:border-gray-700 px-12'>
-              <div className='px-3 py-3 lg:px-5 lg:pl-3'>
-                <div className='flex items-center justify-between'>
-                  <div className='flex items-center justify-start rtl:justify-end'>
-                    <button
-                      onClick={toggleSidebar}
-                      className='inline-flex items-center p-2 text-background rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-                    >
-                      <span className='sr-only'>Open sidebar</span>
-                      <span className='text-foreground p-1'>
-                        {isSidebarOpen ? <IoClose /> : <TfiMenu />}
-                      </span>
-                    </button>
-                    <Link href='/'>
-                      <LogoMain />
-                    </Link>
-                  </div>
-                  <div className='flex items-center'>
-                    <div className='flex items-center ms-3'>
-                      <Dropdown backdrop='blur'>
-                        <DropdownTrigger>
-                          <Button variant='solid' color='primary'>
-                            {'Admin'} <FaChevronDown />
-                          </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu
-                          variant='shadow'
-                          aria-label='Static Actions'
+        // session.status === 'authenticated' && (
+        <>
+          <nav className='fixed top-0 z-50 w-full bg-background border-solid border-gray-300 border-b-1 dark:bg-gray-800 dark:border-gray-700 px-12'>
+            <div className='px-3 py-3 lg:px-5 lg:pl-3'>
+              <div className='flex items-center justify-between'>
+                <div className='flex items-center justify-start rtl:justify-end'>
+                  <button
+                    onClick={toggleSidebar}
+                    className='inline-flex items-center p-2 text-background rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+                  >
+                    <span className='sr-only'>Open sidebar</span>
+                    <span className='text-foreground p-1'>
+                      {isSidebarOpen ? <IoClose /> : <TfiMenu />}
+                    </span>
+                  </button>
+                  <Link href='/'>
+                    <LogoMain />
+                  </Link>
+                </div>
+                <div className='flex items-center'>
+                  <div className='flex items-center ms-3'>
+                    <Dropdown backdrop='blur'>
+                      <DropdownTrigger>
+                        <Button variant='solid' color='primary'>
+                          {'Admin'} <FaChevronDown />
+                        </Button>
+                      </DropdownTrigger>
+                      <DropdownMenu
+                        variant='shadow'
+                        aria-label='Static Actions'
+                      >
+                        <DropdownItem
+                          key='delete'
+                          startContent={<IoLogOut />}
+                          className='text-danger'
+                          color='danger'
+                          onClick={() => handleLogout()}
                         >
-                          <DropdownItem
-                            key='delete'
-                            startContent={<IoLogOut />}
-                            className='text-danger'
-                            color='danger'
-                            onClick={() => handleLogout()}
-                          >
-                            Cerrar sesión
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </Dropdown>
-                    </div>
+                          Cerrar sesión
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
                   </div>
                 </div>
               </div>
-            </nav>
-            <Sidebar isSidebarOpen={isSidebarOpen}>{children}</Sidebar>
-          </>
-        )
+            </div>
+          </nav>
+          <Sidebar isSidebarOpen={isSidebarOpen}>{children}</Sidebar>
+        </>
+        // )
       )}
     </>
   )
